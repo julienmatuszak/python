ff69dfc212ffa603a12093b0d2bc44b9a3def015
julienmatuszak <julienmatuszak@gmail.com>
2018-11-22 11:02:54 +0200

    openclassrooms first version

--- Python Scripting Academy/Python Scripting Academy Unit 1/Module 2/1_2_Required.py
+++
@@ -1,9 +0,0 @@
-# [x] create, call and test fishstore() function 
-def fishstore(fish,price):
-    rtn = "Fish Type: "+ fish + " costs " + price
-    return(rtn)
-
-fish_entry = input("What is the name of the fish ? ")
-price_entry = input("What is the price of the fish ? ")
-print(fishstore(fish_entry,price_entry))
-# then PASTE THIS CODE into edX
